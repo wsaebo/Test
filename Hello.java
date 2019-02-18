f0a8c63b7983ce077fdb4e65f0253ca751121065
unknown <wsaebo@gmail.com>
2019-02-18 11:55:35 +0100

    Fjerna feil

--- Hello.java
+++ Hello.java
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-public class Hello {
-	public static void main (String [] args) {
-		System.out.println("Hello world");
 
-	}
-=======
 public class Hello {
 	//Hello World
 	public static void main (String [] args) {
 		System.out.println("Hello world");
 
 	}
->>>>>>> 6ed5c57bd565aebafcb12c0e2d54253de1bdcaf9
+
 }
